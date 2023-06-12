00451947149d7bc2a716399020c1fa141a2cf0c8
Corendo6 <durrl5897@gmail.com>
2023-06-12 18:42:26 +0900

    06/12

---
+++ minUniqueGames/src/main/java/com/uniquegames/service/OrderService.java
@@ -0,0 +1,37 @@
+package com.uniquegames.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.uniquegames.vo.OrderVo;
+
+public interface OrderService {
+	ArrayList<OrderVo> getCartList(String m_id);
+
+	int getCartCount(String m_id);
+
+	int getCartDeleteAll(String m_id);
+
+	int getCartDeleteOne(int id);
+
+	ArrayList<OrderVo> getOrderList(List<Integer> checkedList);
+
+	int getOrderAmount(List<Integer> checkedList);
+
+	int getOrderComplete(List<Integer> checkedList, String method);
+
+	ArrayList<OrderVo> getPaymentDetail(String m_id, String array);
+
+	int getPaymentCount(String m_id);
+
+	int getPaymentAmount(String m_id);
+
+	ArrayList<OrderVo> getDonationDetail(String c_id, String array);
+
+	int getExpected(String c_id);
+
+	int getTotalDonation(String c_id);
+
+	ArrayList<OrderVo> getDonationRank(String c_id);
+
+}
